7ef35c9c3219392ecd9ec60518e41cc7d7d2090d
liu99002 <liu99002@gmail.com>
2024-02-22 11:59:28 +0800

    2024/2/22

--- cpu2os-master/A1-C語言/00-gcc/01-basic/fib.c
+++ cpu2os-master/A1-C語言/00-gcc/01-basic/fib.c
@@ -1,8 +1,15 @@
-int fib(int n)
-{
-  if (n == 0)
+#include <stdio.h>
+
+int main() {
+    int fib(int n)
+    {
+      if (n == 0)
+        return 0;
+      if (n == 1)
+        return 1;
+      return fib(n - 1) + fib(n - 2);
+    }
     return 0;
-  if (n == 1)
-    return 1;
-  return fib(n - 1) + fib(n - 2);
 }
+
+
