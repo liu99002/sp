00c735e6aa69e0bf8ae2096a62473075ce5a1a12
liu99002 <liu99002@gmail.com>
2024-05-10 21:48:59 +0800

    2024 5 10

--- hw/hw6/server.c
+++ hw/hw6/server.c
@@ -1,34 +1,49 @@
 #include "net.h"
 
 int serv(int connfd) {
-  close(STDOUT_FILENO);                    // 關閉 stdout
-  dup2(connfd, STDOUT_FILENO);             // 用 connfd 取代 stdout
-  dup2(connfd, STDERR_FILENO);             // 用 connfd 取代 stderr
-  while (1) {
-    char cmd[SMAX], fullcmd[SMAX];
-    int len = read(connfd, cmd, SMAX);     // 讀入 client 傳來的命令
-    if (len <= 0) break;                   // 若沒讀到就結束了！
-    strtok(cmd, "\n");                     // 把 \n 去除
-    fprintf(stderr, "cmd=%s\n", cmd);      // 印出命令方便觀察
-    if (strncmp(cmd, "exit", 4)==0) break; // 若是 exit 則離開！
-    snprintf(fullcmd, SMAX, "%s;echo $PWD", cmd); // 將命令加上路徑設定取得指令。
-    system(fullcmd);                       // 執行該命令 (由於 connfd 取代了 stdout，所以命令的輸出會直接傳回給 client)
-  }
-  close(connfd);
-  exit(0);
+    char ipath[SMAX], path[SMAX], cmd[SMAX], fullcmd[SMAX], pathFile[SMAX];
+    int sfd = dup(STDOUT_FILENO);
+    getcwd(ipath, SMAX-1); // 取得初始路徑
+    strcpy(path, ipath);   // path = ipath
+    sprintf(pathFile, "%s/path.txt", ipath); // pathFile=<ipath>/path.txt
+    while (1) { // 不斷等待使用者輸入命令並執行之
+        dup2(sfd, STDOUT_FILENO);
+        dup2(sfd, STDERR_FILENO);
+        printf("myshell:%s $ ", path); // 顯示提示訊息
+        fgets(cmd, SMAX-1, stdin);                     // 等待使用者輸入命令
+        strtok(cmd, "\n");             // 切掉 \n
+        if (strcmp(cmd, "exit")==0) break;
+        sprintf(fullcmd, "cd %s;%s;pwd>%s", path, cmd, pathFile); // fullcmd = 切到 path; 使用者輸入的命令; 將路徑存入 pathFile 中。
+        dup2(connfd, STDOUT_FILENO);             // 用 connfd 取代 stdout
+        dup2(connfd, STDERR_FILENO);             // 用 connfd 取代 stderr
+        system(fullcmd);               // 執行 fullcmd 
+        readText(pathFile, path, SMAX);// 讀 pathFile 檔取得路徑
+        strtok(path, "\n");            // 切掉 \n
+    }
+
+    close(connfd);
+    exit(0);
+}
+
+int readText(char *file, char *text, int size) {
+    FILE *f = fopen(file, "r");
+    int n = fread(text, 1, size, f);
+    fclose(f);
+    return n;
 }
 
 int main(int argc, char *argv[]) {
-  int port = (argc >= 2) ? atoi(argv[1]) : PORT;
-  net_t net;
-  net_init(&net, TCP, SERVER, port, NULL);
-  net_bind(&net);
-  net_listen(&net);
-  while(1) { // 主迴圈：等待 client 連進來，然後啟動 serv 為其服務
-    int connfd = net_accept(&net); // 等待連線進來
-    assert(connfd >= 0);
-    if (fork() <= 0) { // 創建新的行程去服務該連線。
-      serv(connfd);    // 子行程開始執行 serv()
+    int port = (argc >= 2) ? atoi(argv[1]) : PORT;
+    net_t net;
+    net_init(&net, TCP, SERVER, port, NULL);
+    net_bind(&net);
+    net_listen(&net);    
+    while(1) { // 主迴圈：等待 client 連進來，然後啟動 serv 為其服務
+        int connfd = net_accept(&net); // 等待連線進來
+        assert(connfd >= 0);
+        if (fork()==0)
+        {
+            serv(connfd);
+        }
     }
-  }
 }
